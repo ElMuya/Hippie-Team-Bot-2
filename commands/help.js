0841e96f6bc5498027646e46b300a6959ba82a51
ElMuya <85646744+ElMuya@users.noreply.github.com>
2024-05-07 22:17:08 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -11,12 +11,12 @@ module.exports = {
      
 
       const embed = new EmbedBuilder()
-         .setColor('#00b19e')
-      .setTitle('COMANDOS:')
-      .setDescription('Welcome to the Music Bot!\n\n- Here are the available commands:\n\n' +
-        '**/play :** Empiece a reproducir las canciones.\n' +
-        '**/ping :** Comprobar la latencia del bot.\n' +
-        '**/support :** Mostrar información del servidor.');
+        .setColor('#00b19e')
+        .setTitle('COMANDOS:')
+        .setDescription('**/play :** Empiece a reproducir las canciones.\n' +
+          '**/ping :** Comprobar la latencia del bot.\n' +
+          '**/support :** Mostrar información del servidor.')
+        .setImage('https://media.discordapp.net/attachments/1236113159861964930/1236409791631327232/Banner_Discord_Invit.png?ex=6637e7d9&is=66369659&hm=194bdd64fd6cd7e8f627f8782b22c6fc1bed8f4f8668b815c6ee36f501a1da07&=&quality=lossless');
 
       return interaction.reply({ embeds: [embed] });
     } catch (e) {
