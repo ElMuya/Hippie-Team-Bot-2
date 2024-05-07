ec90a5bee1e355a8921a968d4b1490ac62484b9a
ElMuya <85646744+ElMuya@users.noreply.github.com>
2024-05-08 01:10:32 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,21 +1,21 @@
 const { EmbedBuilder } = require('discord.js');
 
+
 module.exports = {
-  name: "support",
-  description: "Obtén información del servidor.",
+  name: "help",
+  description: "Obtén información sobre el bot",
   permissions: "0x0000000000000800",
   options: [],
   run: async (client, interaction) => {
     try {
+     
 
-      const instagramLink = "https://www.instagram.com/hippie.team";
-      const youtubehtLink = "https://www.youtube.com/@hippieteamoficial";
-      const youtubehscLink = "https://www.youtube.com/@hippiesimcup";
-      const webLink = "http://hippieteam.free.nf";
       const embed = new EmbedBuilder()
         .setColor('#00b19e')
-        .setTitle('INFORMACIÓN:')
-        .setDescription(`- **Web -** ${webLink}\n- **Instagram -** ${instagramLink}\n- **Youtube HT -** ${youtubehtLink}\n- **Youtube HSC -** ${youtubehscLink}`)
+        .setTitle('COMANDOS:')
+        .setDescription('**/play :** Empiece a reproducir las canciones.\n' +
+          '**/ping :** Comprobar la latencia del bot.\n' +
+          '**/support :** Mostrar información del servidor.')
         .setImage('https://media.discordapp.net/attachments/1236113159861964930/1236409791631327232/Banner_Discord_Invit.png?ex=6637e7d9&is=66369659&hm=194bdd64fd6cd7e8f627f8782b22c6fc1bed8f4f8668b815c6ee36f501a1da07&=&quality=lossless')
         .setTimestamp()
         .setFooter({ 
